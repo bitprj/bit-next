b6dafad9a68d78410043fc120e47bbe12f11f4ec
wongband <bmwong@ucdavis.edu>
2020-05-20 18:52:21 -0700

    Added Antd and organization page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "../components/common/Layout";
 import ContextProvider from "../lib/context";
 import "../styles.css";
+import 'antd/dist/antd.css';
 
 if (typeof window !== "undefined") {
   require("lazysizes/plugins/attrchange/ls.attrchange.js");
