4049f4dcc779095920025662e4669cd7c6411960
xerilius <gcw.chung@gmail.com>
2020-05-23 03:39:45 +0000

    Added Organization model

--- backend/conduit/organizations/models.py
+++ backend/conduit/organizations/models.py
@@ -6,3 +6,22 @@ from slugify import slugify
 from conduit.database import (Model, SurrogatePK, db, Column,
                               reference_col, relationship)
 from conduit.profile.models import UserProfile
+
+name: string(100)
+slug: text
+description: text
+createdAt: DateTime
+moderators: User relationship, use backref to make relationship.
+members: User relationship, use backref to make relationship.
+
+
+class Organization(Model, SurrogatePK):
+    __tablename__ = 'organization'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = Column(db.String(100))
+    slug = Column(db.Text)
+    description = Column(db.Text)
+    createdAt = Column(db.DateTime)
+    moderators = relationship('User', backref=db.backref('organizations'))
+    members = relationship('User', backref=db.backref('organizations'))
