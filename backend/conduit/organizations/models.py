f5be501fa59e37fcbf7e26393822d29a5ba8dea4
xerilius <gcw.chung@gmail.com>
2020-05-25 13:09:11 +0000

    Added methods to add/remove followers

--- backend/conduit/organizations/models.py
+++ backend/conduit/organizations/models.py
@@ -8,23 +8,27 @@ from conduit.database import (Model, SurrogatePK, db, Column,
 from conduit.profile.models import UserProfile
 from conduit.user.models import User
 
+
 moderator_assoc = db.Table("moderator_assoc",
-                        db.Column("moderators", db.Integer,
-                            db.ForeignKey('userprofile.id'),
-                            primary_key=True),
+                        db.Column("moderator", db.Integer,
+                            db.ForeignKey('userprofile.id')),
                         db.Column("organization", db.Integer,
-                            db.ForeignKey('organization.id'),
-                            primary_key=True)
+                            db.ForeignKey('organization.id'))
                         )
 
-user_assoc = db.Table("user_assoc",
-                    db.Column("members", db.Integer, 
-                        db.ForeignKey('userprofile.id'),
-                        primary_key=True),
+member_assoc = db.Table("member_assoc",
+                    db.Column("member", db.Integer, 
+                        db.ForeignKey('userprofile.id')),
                     db.Column("organization", db.Integer,
-                        db.ForeignKey('organization.id'),
-                        primary_key=True)
+                        db.ForeignKey('organization.id'))
                     )   
+
+follower_assoc = db.Table("follower_assoc",
+                    db.Column("follower", db.Integer, 
+                        db.ForeignKey('userprofile.id')),
+                    db.Column("organization", db.Integer,
+                        db.ForeignKey('organization.id'))
+                    )
 
 
 class Organization(Model, SurrogatePK):
@@ -37,11 +41,32 @@ class Organization(Model, SurrogatePK):
     createdAt = Column(db.DateTime, default=dt.datetime.utcnow)
     moderators = relationship('UserProfile', secondary=moderator_assoc,
                               backref=db.backref('mod_organization'))
-    member_id = reference_col('userprofile', nullable=False)
-    members = relationship('UserProfile', secondary=user_assoc, 
+    members = relationship('UserProfile', secondary=member_assoc, 
                            backref=db.backref('mem_organization'))
+    followers = relationship('UserProfile', secondary=follower_assoc,
+                            backref=db.backref('org_follower'))
 
     # Constructor to take in name, slug & description
     def __init__(self, name, slug=None, description, **kwargs):
         db.Model.__init__(self, name=name, slug=slug or slugify(name), 
                           description=description, **kwargs)
+
+    # Method to allow user to follow organization
+    def follow(self, profile):
+        if not self.is_following(profile):
+            self.followers.append(profile)
+            return True
+        return False
+
+    # Method to allow user to unfollow
+    def unfollow(self, profile):
+        if self.is_following(profile):
+            self.followers.remove(profile)
+            return True
+        return False
+
+    # Method to check if user is already following organization
+    def is_following(self, profile):
+        return bool(self.query.filter(
+                    follower_assoc.c.follower == profile.id)).count())
+    
