90fd452222273bec55d8141d1f70e715921f4be0
Yuan Diao <yuandiao@usc.edu>
2020-05-25 00:37:11 -0700

    Add fields for the Tag model

--- backend/conduit/tags/models.py
+++ backend/conduit/tags/models.py
@@ -8,12 +8,29 @@ from slugify import slugify
 from conduit.database import (Model, SurrogatePK, db, Column,
                               reference_col, relationship)
 
+tag_follower_assoc = db.Table("tag_follower_assoc",
+                     db.Column("tag_id", db.Integer, db.ForeignKey("tags.id"), primary_key=True),
+                     db.Column("follower_id", db.Integer, db.ForeignKey("userprofile.id"), primary_key=True))
+
+tag_moderators_assoc = db.Table("tag_moderators_assoc",
+                     db.Column("tag_id", db.Integer, db.ForeignKey("tags.id"), primary_key=True),
+                     db.Column("moderator_id", db.Integer, db.ForeignKey("userprofile.id"), primary_key=True))
+
 
 class Tags(Model):
     __tablename__ = 'tags'
 
     id = db.Column(db.Integer, primary_key=True)
     tagname = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    slug = db.Column(db.String(100))
+    icon = db.Column(db.String(50))
+    modSetting = db.Column(db.Integer)
+
+    tagFollowers = db.relationship('UserProfile', secondary=tag_follower_assoc, lazy='subquery',
+        backref=db.backref('followed_tags', lazy='dynamic'))
+    moderators = db.relationship('UserProfile', secondary=tag_moderators_assoc, lazy='subquery',
+        backref=db.backref('moderated_tags', lazy='dynamic'))
 
     def __init__(self, tagname):
         db.Model.__init__(self, tagname=tagname)
