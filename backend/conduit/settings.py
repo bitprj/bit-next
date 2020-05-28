c4aaa9f6fc21540e6ffdbe332e4e98d829dfaa12
wongband <bmwong@ucdavis.edu>
2020-05-27 19:30:41 -0700

    Added more testing urls and whitelisted production aand local sites

--- backend/conduit/settings.py
+++ backend/conduit/settings.py
@@ -26,6 +26,7 @@ class Config(object):
         'http://0.0.0.0:4000',
         'http://localhost:4000',
         'http://localhost:3000',
+        'https://bit-next-ixppal0sd.now.sh'
     ]
     JWT_HEADER_TYPE = 'Token'
 
