3bb99c957dcb65bdd17317cb001480ecf7934cdb
xerilius <gcw.chung@gmail.com>
2020-05-29 10:21:07 +0000

    Updated needsReview Column, set default=False

--- backend/conduit/articles/models.py
+++ backend/conduit/articles/models.py
@@ -56,7 +56,7 @@ class Article(SurrogatePK, Model):
     body = Column(db.Text)
     createdAt = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
     updatedAt = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
-    needsReview = Column(db.Boolean, nullable=False)
+    needsReview = Column(db.Boolean, default=False)
     author_id = reference_col('userprofile', nullable=False)
     author = relationship('UserProfile', backref=db.backref('articles'))
     favoriters = relationship(
