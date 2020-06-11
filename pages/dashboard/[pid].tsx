4bb488513c50ffb1892f70ef5d386be5787346ed
Schuyler deVos <schuylerdevos@gmail.com>
2020-06-11 16:50:49 -0400

    Update [pid].tsx

--- pages/dashboard/[pid].tsx
+++ pages/dashboard/[pid].tsx
@@ -24,19 +24,11 @@ const StyledMenu = styled(Menu)`
 `
 
 const Profile = ({ initialProfile }) => {
-	{/*so this is setting the router up and then calling query: pid from the router,
-		which is just the pid which is just the username
-		query is the data type because, you know, typescript*/}
 	const router = useRouter();
 	const {
 		query: { pid },
 	} = router;
 
-	{/*right, and now the fetched and initial profile are matching (or not
-	if you're not the user)*/}
-	{/*the imported fetcher function checks to see if there's a user in localstorage,
-	and if there is whether there's an authentification token. if there is it sends a
-	get request with the authorization tokens*/}
 	const {
 		data: fetchedProfile,
 		error: profileError,
@@ -48,41 +40,24 @@ const Profile = ({ initialProfile }) => {
 
 	if (profileError) return <ErrorMessage message="Can't load profile" />;
 
-	{/*"if you couldnt fetch anything, use the initial one you got"*/}
 	const { profile } = fetchedProfile || initialProfile;
-	{/*grabbing the data from the profile*/}
 	const { username, bio, image, following } = profile;
-	{/*this is usestate, so it stores the state in the first variable (state as given)
-	and then the second variable is what you're gonna call when you want to change
-	the state*/}
 	const [list, setList] = React.useState(["Posts", "Followers", "Following", "Account Settings"])
 	const [tab_select_list, setTabList] = React.useState(["All Posts", "Published", "Drafts"])
-	{/*this is being set to true because it's the first thing to show*/}
 	const [isPosts, setPostsPage] = React.useState(true)
-	{/*the rest don't show immediately so they're set to false*/}
 	const [isFollowers, setFollowersPage] = React.useState(false)
 	const [isFollowings, setFollowingsPage] = React.useState(false)
 	const [isTag, setTagPage] = React.useState(false)
 	const [isSettings, setSettingsPage] = React.useState(false)
-
-	{/*these were mine but don't seem necessary anymore*/}
 	const [isAllArticles, setAllArticles] = React.useState(true);
 	const [isPublished, setPublished] = React.useState(false);
 	const [isDrafts, setDrafts] = React.useState(false);
-
-	{/*this just uses getItem by key from localstorage and sets currentUser to that*/}
 	const { data: currentUser } = useSWR("user", storage);
-	{/*checklogin checks to see if the useSWR call above actually returned a user
-		since obviously if it didnt it'd mean nobody's home*/}
 	const isLoggedIn = checkLogin(currentUser);
-	{/*this is just checking that... the currentuser as defined above matches
-		the user fetched from getInitialProps*/}
 	const isUser = currentUser && username === currentUser?.username;
 
-	{/*tab thing from ant design*/}
 	const { TabPane } = Tabs;
 
-	{/*this is the function to change which list it's gonna show*/}
 	const TabChange = (key) => {
 		if (key == "Posts") {
 			setTabList(["All Posts", "Published", "Drafts"])
@@ -120,7 +95,6 @@ const Profile = ({ initialProfile }) => {
 			setTagPage(false)
 			setSettingsPage(true)
 		}
-		//seems like this will never call tags
 		else {
 			setTabList(["Most Viewed", "Most Liked", "Most Recent"])
 			setPostsPage(false)
@@ -131,7 +105,6 @@ const Profile = ({ initialProfile }) => {
 		}
 	}
 
-	{/*this was for drafts and published, but it seems like it's not needed now*/}
 	const TabView = (key) => {
 	 	if (key == "All Posts") {
 			setAllArticles(true);
@@ -156,7 +129,6 @@ const Profile = ({ initialProfile }) => {
 	}
 
 	if (isUser) {
-		//and here are the goods
 		return (
 			<Row gutter={16} style={{ marginTop: "10%", marginLeft: "0", marginRight: "0" }}>
 				<Col span={2}></Col>
@@ -199,7 +171,6 @@ const Profile = ({ initialProfile }) => {
 	}
 };
 
-{/*this is the, uh, the info of the logged in user*/}
 Profile.getInitialProps = async ({ query: { pid } }) => {
 	const { data: initialProfile } = await UserAPI.get(pid);
 	return { initialProfile };
