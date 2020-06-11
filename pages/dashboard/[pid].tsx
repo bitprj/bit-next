cf9704d343cbb75e7986f3c699c805bf20a50023
Schuyler deVos <schuylerdevos@gmail.com>
2020-06-11 16:47:31 -0400

    Update [pid].tsx

--- pages/dashboard/[pid].tsx
+++ pages/dashboard/[pid].tsx
@@ -5,6 +5,7 @@ import fetcher from "../../lib/utils/fetcher";
 import storage from "../../lib/utils/storage";
 import { SERVER_BASE_URL } from "../../lib/utils/constant";
 import UserAPI from "../../lib/api/user";
+import ArticleAPI from "../../lib/api/article";
 import checkLogin from "../../lib/utils/checkLogin";
 
 import ArticleList from "../../components/article/ArticleList";
@@ -17,19 +18,25 @@ import { Row, Col, Tabs, Menu } from 'antd';
 
 import styled from "styled-components";
 
-import { useSession, getSession } from 'next-auth/client';
-
 const StyledMenu = styled(Menu)`
 	font-size: 15px;
 	font-weight: bold;
 `
 
 const Profile = ({ initialProfile }) => {
+	{/*so this is setting the router up and then calling query: pid from the router,
+		which is just the pid which is just the username
+		query is the data type because, you know, typescript*/}
 	const router = useRouter();
 	const {
 		query: { pid },
 	} = router;
 
+	{/*right, and now the fetched and initial profile are matching (or not
+	if you're not the user)*/}
+	{/*the imported fetcher function checks to see if there's a user in localstorage,
+	and if there is whether there's an authentification token. if there is it sends a
+	get request with the authorization tokens*/}
 	const {
 		data: fetchedProfile,
 		error: profileError,
@@ -41,30 +48,41 @@ const Profile = ({ initialProfile }) => {
 
 	if (profileError) return <ErrorMessage message="Can't load profile" />;
 
-	//we're getting something from the api, but it doesn't seem like it's returning
-	//everything it should return
+	{/*"if you couldnt fetch anything, use the initial one you got"*/}
 	const { profile } = fetchedProfile || initialProfile;
+	{/*grabbing the data from the profile*/}
 	const { username, bio, image, following } = profile;
-	//this is usestate, so it stores the state in the first variable (state as given)
-	//and then the second variable is what you're gonna call when you want to change
-	//the state
+	{/*this is usestate, so it stores the state in the first variable (state as given)
+	and then the second variable is what you're gonna call when you want to change
+	the state*/}
 	const [list, setList] = React.useState(["Posts", "Followers", "Following", "Account Settings"])
 	const [tab_select_list, setTabList] = React.useState(["All Posts", "Published", "Drafts"])
-	//this is being set to true because it's the first thing to show
+	{/*this is being set to true because it's the first thing to show*/}
 	const [isPosts, setPostsPage] = React.useState(true)
-	//the rest don't show immediately so they're set to false
+	{/*the rest don't show immediately so they're set to false*/}
 	const [isFollowers, setFollowersPage] = React.useState(false)
 	const [isFollowings, setFollowingsPage] = React.useState(false)
 	const [isTag, setTagPage] = React.useState(false)
 	const [isSettings, setSettingsPage] = React.useState(false)
 
-	//k, this is that one that stays in lokalstorage
+	{/*these were mine but don't seem necessary anymore*/}
+	const [isAllArticles, setAllArticles] = React.useState(true);
+	const [isPublished, setPublished] = React.useState(false);
+	const [isDrafts, setDrafts] = React.useState(false);
+
+	{/*this just uses getItem by key from localstorage and sets currentUser to that*/}
 	const { data: currentUser } = useSWR("user", storage);
+	{/*checklogin checks to see if the useSWR call above actually returned a user
+		since obviously if it didnt it'd mean nobody's home*/}
 	const isLoggedIn = checkLogin(currentUser);
+	{/*this is just checking that... the currentuser as defined above matches
+		the user fetched from getInitialProps*/}
 	const isUser = currentUser && username === currentUser?.username;
+
+	{/*tab thing from ant design*/}
 	const { TabPane } = Tabs;
 
-	//this tells it which list its gonna show
+	{/*this is the function to change which list it's gonna show*/}
 	const TabChange = (key) => {
 		if (key == "Posts") {
 			setTabList(["All Posts", "Published", "Drafts"])
@@ -73,6 +91,10 @@ const Profile = ({ initialProfile }) => {
 			setFollowingsPage(false)
 			setTagPage(false)
 			setSettingsPage(false)
+
+			setAllArticles(true);
+			setDrafts(false);
+			setPublished(false);
 		}
 		else if (key == "Followers") {
 			setTabList(["Old -> New", "New -> Old"])
@@ -108,9 +130,32 @@ const Profile = ({ initialProfile }) => {
 			setSettingsPage(false)
 		}
 	}
-	const TabView = (key) => { }
 
-	if (true) {
+	{/*this was for drafts and published, but it seems like it's not needed now*/}
+	const TabView = (key) => {
+	 	if (key == "All Posts") {
+			setAllArticles(true);
+			setDrafts(false);
+			setPublished(false);
+		}
+		else if (key == "Published") {
+			setAllArticles(false);
+			setDrafts(false);
+			setPublished(true);
+		}
+		else if (key == "Drafts") {
+			setAllArticles(false);
+			setDrafts(true);
+			setPublished(false);
+		}
+		else {
+			setAllArticles(false);
+			setDrafts(false);
+			setPublished(false);
+		}
+	}
+
+	if (isUser) {
 		//and here are the goods
 		return (
 			<Row gutter={16} style={{ marginTop: "10%", marginLeft: "0", marginRight: "0" }}>
@@ -133,12 +178,9 @@ const Profile = ({ initialProfile }) => {
 							<Tab_list tabs={tab_select_list} onClick={key => TabView(key)} position={"top"} />
 						</Col>
 						<Col span={24} style={{ paddingTop: "0" }}>
-						{/*can either modify all the lists here by passing in the tab parameter
-							as props. cause the end goal is to sort the articles, but right
-							now it just imports the entire article list. actually, what props get passed
-							to article list?
-							article card is the one that has all the props*/}
-							{isPosts ? <ArticleList /> : null}
+							{isPosts && isAllArticles ? <ArticleList /> : null}
+							{isPosts && isPublished ? <div>published stuff</div> : null}
+							{isPosts && isDrafts ? <div>Drafts here</div> : null}
 							{isFollowers ? <FollowList followings={false} /> : null}
 							{isFollowings ? <FollowList followings={true} /> : null}
 							{isTag ? <ArticleList /> : null}
@@ -157,6 +199,7 @@ const Profile = ({ initialProfile }) => {
 	}
 };
 
+{/*this is the, uh, the info of the logged in user*/}
 Profile.getInitialProps = async ({ query: { pid } }) => {
 	const { data: initialProfile } = await UserAPI.get(pid);
 	return { initialProfile };
