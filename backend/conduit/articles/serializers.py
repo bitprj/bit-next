d063569a62000c4aa4790ae63b747d921153ab15
anuragunni <anurag.unnikrishnan@gmail.com>
2020-06-24 00:23:12 -0700

    Acknowledged Aishwaryas request to identify if an article has been bookmarked by the current  user

--- backend/conduit/articles/serializers.py
+++ backend/conduit/articles/serializers.py
@@ -27,6 +27,7 @@ class ArticleSchema(Schema):
     commentsCount = fields.Int(dump_only=True)
     favorited = fields.Bool(dump_only=True)
     isPublished = fields.Bool()
+    bookmarked = fields.Bool(dump_only=True)
     views = fields.Int(dump_only=True)
 
     @pre_load
