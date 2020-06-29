9868c80d7e818b57b67a3d58618acd58d02ffdc6
Ayush Bharat Jain <60114945+ayushbha@users.noreply.github.com>
2020-06-29 14:39:45 -0700

    Changed FormItem component

--- components/profile/Form_Items.tsx
+++ components/profile/Form_Items.tsx
@@ -7,12 +7,12 @@ import styled from "styled-components";
 import { SERVER_BASE_URL } from "../../lib/utils/constant";
 import { Avatar, Row, Col, Input, Form, Button, message } from 'antd';
 
-const Form_Items = (props) => {
+const FormItem = (props) => {
 	return(
 		<div>
-		{props.Item.map(item => <Form.Item label={item[0]} name={item[1]} rules={item[2]}><Input placeholder={item[3]} onChange={props.onChange(item[4])}/></Form.Item>)}
+		<Form.Item label={props.label} name={props.name} rules={props.rules}><Input placeholder={props.placeholder} onChange={props.onChange}/></Form.Item>
 		</div>
 	);
 };
 
-export default Form_Items;
+export default FormItem;
